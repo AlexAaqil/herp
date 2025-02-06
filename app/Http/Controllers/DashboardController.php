f84dd8a0462ca01c9eb215cd4d926185b2ef686c
alexaaqil <alexaaqil.se@gmail.com>
2025-02-06 18:07:22 +0300

    Dashboard: added the count for classes to the dashboard.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,26 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Message;
+use App\Models\Classrooms\Classroom;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $user = Auth()->user();
-        $count_users = User::whereNotIn('user_level', [0, 1])
+        $count_users = User::whereNotIn('user_level', [2])
+            ->where('user_status', 1)
+            ->count();
+        $count_teachers = User::where('user_level', 2)
             ->where('user_status', 1)
             ->count();
         $count_all_users = User::count();
-        $messages = Message::latest()
+
+        $count_classrooms = Classroom::count();
+
+        $unread_messages = Message::latest()
             ->where('status', 0)
             ->take(5)
             ->get();
+        $count_unread_messages = count($unread_messages);
 
         return view('dashboard.index', compact(
             'user',
             'count_users',
+            'count_teachers',
             'count_all_users',
-            'messages',
+
+            'count_classrooms',
+
+            'unread_messages',
+            'count_unread_messages',
         ));
     }
 }
