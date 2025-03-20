9772e62d1d4533ece89e5e46a8530487c8408aa2
AlexAaqil <alexaaqil.se@gmail.com>
2025-03-20 12:38:28 +0300

    Config: updated the name of the app.

--- config/globals.php
+++ config/globals.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'app_name' => 'Optima International High School',
-    'app_acronym' => 'O.I.H.S',
+    'app_name' => 'Optima High School MS',
+    'app_acronym' => 'O.H.S.M.S',
     'app_phone_number' => '+254 746 055 487',
     'app_phone_other' => '+254 745 744 261',
     'app_whatsapp_number' => '254746055487',
